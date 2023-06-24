50c0719fd538c26493a9bbb9143c70272b7eb01d
jugantar <jugantarroy27@gmail.com>
2023-06-24 13:53:19 +0530

    modified:   my-app/src/components/Navbar.js

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Navbar</a>
+    <a class="navbar-brand" href="/">Medico</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
