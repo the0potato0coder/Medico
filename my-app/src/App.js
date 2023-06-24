007167dbbcd3024bf0d1582d42b4b3430d12fbb7
jugantar <jugantarroy27@gmail.com>
2023-06-24 14:03:51 +0530

    modified:   my-app/src/App.js 	modified:   my-app/src/components/Navbar.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Home from './components/Home';
 function App() {
   return (
     <>
     <Navbar/>
+    <Home/>
     </>
   );
 }
