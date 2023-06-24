0760c05aa2447c2e7e2c5e06952456c492ce2445
jugantar <jugantarroy27@gmail.com>
2023-06-25 01:45:06 +0530

    modified:   my-app/src/pages/About/About.js 	modified:   my-app/src/App.js 	modified:   my-app/src/components/Navbar.js 	modified:   my-app/src/pages/Homepage/Home.js

--- my-app/src/pages/About/About.js
+++ my-app/src/pages/About/About.js
@@ -3,6 +3,19 @@ import './About.css';
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+        <p className=''>
+        Medico is the premium expert opinion platform that provides users (Global Indians or anyone away from their hometown) with personalized advice and guidance on important matters.
+        </p>
+        <p>
+        Medico panel of experts includes highly qualified and experienced doctors, lawyers, chartered accountants, architects, designers, and other rare consultants. Great care is taken in expanding their team by regularly onboarding new top-of-the-line consultants, doctors, and experts who undergo a rigorous validation process, including referrals and vetting. Users can consult human experts who possess deep knowledge in their respective fields, gaining access to expert insights, alternative perspectives, and critical analysis that contribute to well-informed decision-making.
+        </p>
+        <p>
+        Through Medico websites and apps, users gain access to comprehensive expert profiles, convenient appointment scheduling, and flexible options for video, audio, or chat consultations. With integrated payment capabilities and a satisfaction rating system, our platform eliminates the need for travel, reduces risks, and saves valuable time and money for our users.
+        </p>
+        <p>
+        Medico team's human-centric approach emphasizes compassion and care, ensuring that individuals receive support at the earliest. To provide a seamless experience, they offer case managers who help users even if it requires in-person consultations. The Medico experts offer personalized guidance tailored to the user's unique needs and circumstances. By leveraging the competence of human professionals, Medico bridges the gap between specialized knowledge and actionable next steps.
+        </p>
+    </div>
   )
 }
