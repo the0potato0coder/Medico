a120a0d8a79923393d3f4ad4093ac800815f3048
jugantar <jugantarroy27@gmail.com>
2023-06-25 01:20:17 +0530

    modified:   my-app/src/App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import Home from './pages/Homepage/Home';
-import Test from './pages/page2/Test';
 import About from './pages/About/About';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -10,7 +9,7 @@ function App() {
     <Navbar/>
     <BrowserRouter>
     <Routes>
-        <Route path='/Test' element={<Test/>}/>
+        
         <Route path='/' element={<Home />}/>
         <Route path='/About' element={<About />}/>
       </Routes>
