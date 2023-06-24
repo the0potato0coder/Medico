94c70aa3a4fae0ffd96315964409bbc04375ff30
jugantar <jugantarroy27@gmail.com>
2023-06-25 01:19:23 +0530

    modified:   my-app/src/App.js 	modified:   my-app/src/components/Navbar.js     new file:   my-app/src/pages/About/About.css 	new file:   my-app/src/pages/About/About.js 	modified:   my-app/src/pages/Homepage/Home.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
 import Home from './pages/Homepage/Home';
+import Test from './pages/page2/Test';
+import About from './pages/About/About';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <> 
+    <Navbar/>
     <BrowserRouter>
     <Routes>
+        <Route path='/Test' element={<Test/>}/>
         <Route path='/' element={<Home />}/>
+        <Route path='/About' element={<About />}/>
       </Routes>
     </BrowserRouter>
      
